56e1506e1c45547b7650495bde68f26208c2f5fb
Jualns <josilva602@gmail.com>
2022-09-15 14:10:17 -0300

    feat: add a main.rs the functions to create a wallet, mine, start the 3 systems, send and receive with HTTP method amd close all systems

--- src/main.rs
+++ src/main.rs
@@ -1,95 +1,175 @@
-// std
-use std::{process::{Command, Child, Output}, array, str::Split, path::PathBuf};
-//use std::f32;
-//use std::env;
-//use std::fs::remove_dir_all;
+//use std::fmt::Display;
+use std::{thread, time};
+use std::time::Duration;
+//use std::sync::mpsc;
+use std::{process::Child, fs::remove_file};
+use std::convert::Infallible;
+use std::time::Instant;
+//use std::process::{Command, Output};
 
-// Testing
-use testing::{spawn_network, wait_for, spawn_wallet_listen, create_wallet, send_coins_smallest,
-    spawn_miner, get_number_transactions_txs, info_wallet, receive_finalize_coins,
-    generate_response_file_name, generate_file_name};
-use testing::get_test_configuration;
+//Testing
+use testing::{
+            wait_for,
+            get_test_configuration,
+            spawn_network,
+            create_wallet,
+            str_to_chain_type,
+            spawn_miner, spawn_wallet_listen,
+            get_passphrase,
+            send_coins_smallest,
+            confirm_transaction,
+            info_wallet,
+            new_child,
+            //new_output,
+            get_number_transactions_txs,
+            get_http_wallet,
+            receive_finalize_coins,
+            generate_file_name,
+            generate_response_file_name,
+            };
 
-
-// Epir Server
+// Epic Server
 use epic_core::global::ChainTypes;
-//use epic_config::GlobalConfig;
 
-//#[derive(Serialize)]
-//struct Server_Toml {
-//    pub run_tui: Option<bool>,
-//    pub stdout_log_level: Level,
-//    pub file_log_level: Level,
-//    pub stratum_server_addr: Option<String>,
-//    pub seeding_type: Seeding,
-//    pub seeds: Option<Vec<PeerAddr, Global>>,
+//Epic Wallet
+//use epic_wallet_config::config::initial_setup_wallet;
+
+//impl fmt::Debug for TransWorld {
+//    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//        write!(f, "chain_type :{:?}", self.wallet_binary)
+//    }
 //}
 
-#[allow(unused_variables)]
+//impl fmt::Debug for WalletInformation {
+//    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//        write!(f, "chain_type :{:?}", self.sent_tx)
+//    }
+//}
+
+impl std::default::Default for BigWalletWorld {
+	fn default() -> BigWalletWorld {
+		BigWalletWorld {
+            chain_type: ChainTypes::UserTesting,
+            send_method: String::from("http"),
+            http_path: String::new(),
+            password: String::from("1"),
+            server_binary: String::new(),
+            wallet_binary: String::new(),
+            miner_binary: String::new(),
+            server: new_child(),
+            wallet: new_child(),
+            miner: new_child(),
+		}
+	}
+}
+
+#[derive(Debug)]
+pub struct BigWalletWorld {
+    pub chain_type: ChainTypes,
+    pub send_method: String,
+    pub http_path: String,
+    pub server: Child,
+    pub wallet: Child,
+    pub miner: Child,
+    pub password: String,
+    pub server_binary: String,
+    pub wallet_binary: String,
+    pub miner_binary: String,
+}
+
+pub struct PackTransaction {
+    pub number_transactions: u32,
+    pub duration_time: Vec<Duration>,
+    pub vec_amount: Vec<String>,
+}
+
+//I finalize the emoji transaction
+
+
+
+////////////////////////////////////////////////
+/// 
+/// 
+/// 
+/// 
+/// 
+
+fn have_coins_in_wallet(world: &mut BigWalletWorld) -> bool {
+    let info = info_wallet(&world.chain_type, &world.wallet_binary, &world.password);
+    let current_spendable = info.last().expect("Can't get the current spendable!");
+    current_spendable > &0.0
+}
+
+//#[tokio::main]
 fn main() {
-    let chain_type = ChainTypes::UserTesting;
-    let password = "1";
+    let method_to_sent = String::from("http");
 
-    get_test_configuration(&chain_type);
+    //// Init the world
+    let mut big_wallet = BigWalletWorld::default();
+
+    //// Init the variables    
+    big_wallet.chain_type = ChainTypes::UserTesting;
+    big_wallet.server_binary = String::from("/home/jualns/Desktop/epic/target/release/epic");
+    big_wallet.wallet_binary = String::from("/home/jualns/Desktop/epic-wallet/target/release/epic-wallet");
+    big_wallet.miner_binary = String::from("/home/jualns/Desktop/epic-miner/target/debug/epic-miner");
     
+    //// Init the systems
+
+    // config epic-server.toml with custom configuration
+    get_test_configuration(&big_wallet.chain_type);
+    // Wait the epic-servet.toml save
     wait_for(5);
+    // run wallet and save on world
+    _ = create_wallet(&big_wallet.chain_type, big_wallet.wallet_binary.as_str(), big_wallet.password.as_str());
+    big_wallet.http_path =  get_http_wallet();
+    // run server and save on world
+    big_wallet.server = spawn_network(&big_wallet.chain_type, big_wallet.server_binary.as_str());
+    // save the wallet_listen process on world
+    big_wallet.wallet = spawn_wallet_listen(&big_wallet.chain_type, big_wallet.wallet_binary.as_str(), big_wallet.password.as_str());
+    // Run the miner
+    big_wallet.miner = spawn_miner(&big_wallet.miner_binary);
 
-    let server_binary = "C:\\Users\\T-Gamer\\Desktop\\Brick\\EpicCash\\epic\\target\\release\\epic.exe";//"/home/ba/Desktop/EpicV3/epic/target/release/epic";
-    let wallet_binary = "C:\\Users\\T-Gamer\\Desktop\\Brick\\EpicCash\\epic-wallet\\target\\release\\epic-wallet.exe";//"/home/ba/Desktop/EpicV3/epic-wallet/target/release/epic-wallet";
-    let miner_binary = "C:\\Users\\T-Gamer\\Desktop\\Brick\\EpicCash\\epic-miner\\epic-miner.exe";//"/home/ba/Desktop/epic-miner/target/debug/epic-miner";
-    let mut serv = spawn_network(&chain_type, server_binary);
+    // prepare the pack of transactions
+    let mut now = time::Instant::now();
+    let mut pack_transactions = PackTransaction {
+        number_transactions: 1,
+        duration_time: Vec::new(), //vec![now.elapsed(); 1],
+        vec_amount: Vec::new(), //vec!["1.0".to_string()],
+    };
+    pack_transactions.duration_time.push(now.elapsed());
+    pack_transactions.vec_amount.push("1.0".to_string());
 
-    let created_wallet = create_wallet(&chain_type, wallet_binary, password);
-    //let mut te = Command::new(server_binary).args(["--usernet", "--onlyrandomx"]).spawn().expect("msg");
 
-    wait_for(4);
+    // check if have coins
+    let mut coins_in_wallet = have_coins_in_wallet(&mut big_wallet);
+    while !coins_in_wallet {
+        wait_for(1);
+        coins_in_wallet = have_coins_in_wallet(&mut big_wallet);
+    }
 
-    let mut listen = spawn_wallet_listen(&chain_type, wallet_binary, password);
+    let mut amount: String = pack_transactions.vec_amount.first().expect("Can't have amount to send").to_string();
+    for t_k in 0..pack_transactions.number_transactions as usize {
+        amount = pack_transactions.vec_amount[t_k].to_string();
+        now = time::Instant::now();
+        send_coins_smallest(&big_wallet.chain_type, &big_wallet.wallet_binary, method_to_sent.to_owned(), &big_wallet.password, amount, &big_wallet.http_path);
+        pack_transactions.duration_time.push(now.elapsed());
+    }
 
-    wait_for(5);
+    // Save the pack_transactions
+    // TODO
+        // TO SAVE THE STEP IN FILE https://doc.rust-lang.org/std/fmt/trait.Display.html
+        // Then use fs::File to persist to the filesystem
 
-    let mut miner = spawn_miner(miner_binary);
-    wait_for(180);
 
-    let file_name = generate_file_name();
-    let response_file_name = generate_response_file_name(&file_name);
+    //// Run cucumber steps
+    //futures::executor::block_on(TransWorld::run("./features/transactions.feature"));
 
-    let a = send_coins_smallest(&chain_type, &wallet_binary.to_string(), "file".to_string(), &"1".to_string(), String::from("1"), &file_name);
+    //// Confirm transactions
+    confirm_transaction(&big_wallet.chain_type, &big_wallet.wallet_binary, &big_wallet.password);
 
-    let b = receive_finalize_coins(&chain_type, &wallet_binary.to_string(), "file".to_string(), &"1".to_string(), &"receive".to_string(),&file_name);
-
-    let c = receive_finalize_coins(&chain_type, &wallet_binary.to_string(), "file".to_string(), &"1".to_string(), &"finalize".to_string(),&response_file_name);
-
-    println!("====== SEND COINS {:?} ====== \n", a);
-
-    println!("\n ====== RECEIVED COINS {:?} ====== \n", b);
-
-    println!("\n ====== FINALIZED COINS {:?} ====== \n", c);
-
-    // let info_str = String::from_utf8_lossy(&info.stdout).into_owned();
-    // let info_split = info_str.split(' ').collect::<Vec<&str>>();
+    //// Finish all systems
     
-
-    // let values: Vec<f32> = info_split
-    //                             .into_iter()
-    //                             .flat_map(|x| x.parse::<f32>())
-    //                             .collect();
-    //                             //     if x.contains(".") {
-    //                             //         x
-    //                             //     } else {
-    //                             //         ""
-    //                             //     }
-    //                             // })
-    //                             //.collect();
-
-    //println!("INFO {:#?} \n", info_split);
-    // println!("INFO0 {:#?} \n", info_str.split(&['\n','|',' ']).collect::<Vec<&str>>());
-    // println!("COLLECT {:#?} \n", values);
-
-    
-    listen.kill().expect("Failed on kill wallet");
-    serv.kill().expect("Failed on kill server");
-    miner.kill().expect("Failed on kill miner");
-
-    println!("---------- END OF TEST ----------")
-} 
+    big_wallet.miner.kill().expect("Can't kill miner!");
+    big_wallet.server.kill().expect("Can't kill server!");
+    big_wallet.wallet.kill().expect("Can't kill wallet!");    
+}
