287cb36556e75a321c907a9d5248403b4e529ebf
Jualns <josilva602@gmail.com>
2023-02-22 10:20:34 -0300

    fix: change the qr folder name and gitignore

--- shell/qr-test/stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# File to clean all the processes and files created during a execution of the test
-
-source ./variables.sh
-
-# Kill the epic processes
-ps aux | grep epic | awk '{print $2}' | xargs kill -9
